197023775dc64311106bdeee35a21f6290786600
Débora <deboracristinapereira123@gmail.com>
2020-10-09 04:46:59 -0300

    Validar formulário e react toasting

--- project/src/components/Login/Login.js
+++ project/src/components/Login/Login.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import "./login.css";
 import { Form, Input, Button, Segment } from "semantic-ui-react";
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+toast.configure()
+
+/* Objeto estado inicial */
+
+const InitialState = {
+    username: "",
+    password: "",
+    usernameError: "",
+    passwordError: ""
+}
+
+/* Customizar Toast. Inicial maiúscula. */
+
+const ToastPassword = () => {
+    return (
+        <div>A senha deve ter, no mínimo, 5 caracteres.</div>
+    )
+}
+
+const ToastUsername = () => {
+    return (
+        <div>O campo "Usuário" está incorreto e deve estar em formato de Email (@, .com).</div>
+    )
+}
+
 
 class Login extends Component {
     constructor() {
         super()
-        this.state = {
-            username: "",
-            password: ""
-        }
+        this.state = InitialState
     }
+
+    /* Métodos:
+    Prevent Default: direcionar submit ao método criado */
 
     changeUsername = e => {
         this.setState({ username: e.target.value })
@@ -19,14 +47,47 @@ class Login extends Component {
         this.setState({ password: e.target.value })
     }
 
-    submitForm = e => {
-        if (this.state.password.length <= 4) {
-            alert("A senha deve ter mais do que 4 caracteres.")
-            console.log("Senha: " + this.state.password)
+    validateForm = () => {
+        let usernameError = "";
+        let passwordError = "";
+
+        /* Se não for válido, setar valor do state, exibir warning e retornar false */
+
+        if (this.state.password.length < 5) {
+            passwordError = "Senha inválida."
+            toast.warning(<ToastPassword />, {position: toast.POSITION.TOP_LEFT, autoClose: false})
+            this.setState({ passwordError })
+            return false;
+        } 
+        
+        if (!this.state.username.includes("@") || !this.state.username.includes(".com")) {
+            usernameError = "Usuário inválido."
+            toast.warning(<ToastUsername />, {position: toast.POSITION.TOP_LEFT, autoClose: false})
+            this.setState({ usernameError })
+            return false;
         }
+
+        return true;
+    }
+
+    submitForm = e => {
+        const isValid = this.validateForm();
+
+        if (isValid) {
+            // Limpar formulário
+
+            this.setState(InitialState);
+
+            // Ir para listas
+        }
+
+        // 
+        
 
         e.preventDefault()
     }
+
+    /* Dentro de segments, é possível aplicar flexbox. */
 
     render() {
         return (
